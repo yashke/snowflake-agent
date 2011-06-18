a1834cc6cd76a8261e5aebbb78b8133ae0ace86c
Marta Anna Ziobro <gyarill@gmail.com>
2011-06-18 15:00:17 +0200

    Some tests of position and molecude tbc...

---
+++ CrystalsTests/Assert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace CrystalsTests
+{
+    public static class AssertExtensions
+    {
+        public static double DOUBLE_DELTA = 0.00000000001;
+     
+        public static void AreAlmostEqual(double expected, double actual)
+        {
+            Assert.Less(Math.Abs(expected - actual), DOUBLE_DELTA, String.Format("Expected: {0} Actual:{1}", expected, actual));
+        }
+
+    }
+}
---
+++ CrystalsTests/PositionTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Crystals;
+
+namespace CrystalsTests
+{
+    [TestFixture]
+    public class PositionTests
+    {
+        [Test]
+        public void TetrahedronPartTest()
+        {
+            Position zero = new Position(3.45, 7.93, null);
+            Position p = new Position(3.45, 10, null);
+
+            Assert.AreEqual(1, p.TetrahedronPart(zero));
+
+            p.X = -100;
+            p.Y = 8;
+
+            Assert.AreEqual(2, p.TetrahedronPart(zero));
+
+
+            p.X = 100;
+            p.Y = 1;
+
+            Assert.AreEqual(5, p.TetrahedronPart(zero));
+
+        }
+
+        [Test]
+        public void PointOfAngleTest()
+        {
+            var zero = new Position(0, 0, null);
+            var p = zero.PointOfAngle(Molecule.TETRAHEDRON_SITE, Math.PI * 5 / 6);
+            Assert.Less(p.X, zero.X);
+
+            Assert.AreEqual(2, p.TetrahedronPart(zero));
+        }
+
+        [Test]
+        public void PointOnBorderOfTetrahedronPartTest()
+        {
+            Position zero = new Position(3.45, 7.93, null);
+
+            Double r = 10;
+
+            var p = zero.PointOnBorderOfTetrahedronPart(r, 3);
+
+            var p_expected = new Position(zero.X - r, zero.Y, null);
+
+            AssertExtensions.AreAlmostEqual(p_expected.X, p.X);
+
+            AssertExtensions.AreAlmostEqual(p_expected.Y, p.Y);
+
+            p = zero.PointOnBorderOfTetrahedronPart(r, 5);
+
+            p_expected = new Position(zero.X + r / 2, zero.Y - r * Math.Sqrt(3) / 2, null);
+
+            AssertExtensions.AreAlmostEqual(p_expected.X, p.X);
+
+            AssertExtensions.AreAlmostEqual(p_expected.Y, p.Y);
+
+        }
+    }
+}
