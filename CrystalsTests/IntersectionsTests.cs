48403f3fec6c49c6c1d95e49acb6091b3ac65d1b
Marta Anna Ziobro <gyarill@gmail.com>
2011-06-19 12:43:30 +0200

    Interstctions adjustment and tests

---
+++ CrystalsTests/IntersectionsTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Crystals;
+
+namespace CrystalsTests
+{
+    [TestFixture]
+    class IntersectionsTests
+    {
+        [Test]
+        public void CircleAndSegmentIntersectionsTest()
+        {
+            var positions = Intersections.CircleAndSegmentIntersections(new Position(6, 6), new Position(6, 20), 10, new Position(0, 0));
+
+            var pExp = new Position(6, 8);
+
+            AssertExtensions.AreAlmostEqual(pExp, positions[0]);
+
+            positions = Intersections.CircleAndSegmentIntersections(new Position(0, -8), new Position(12, -8), 10, new Position(0, 0));
+
+            pExp = new Position(6, -8);
+
+            AssertExtensions.AreAlmostEqual(pExp, positions[0]);
+
+            positions = Intersections.CircleAndSegmentIntersections(new Position(0, -7.8), new Position(12, -8.2), 10, new Position(0, 0));
+
+            pExp = new Position(6, -8);
+
+            AssertExtensions.AreAlmostEqual(pExp, positions[0]);
+
+        }
+    }
+}
